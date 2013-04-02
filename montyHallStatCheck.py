82c657451658ddc51011f94c29d88a174e05ea98
Sriram Padmanabhan <spzone@gmail.com>
2013-04-03 01:50:48 +0530

    Update montyHallStatCheck.py

--- montyHallStatCheck.py
+++ montyHallStatCheck.py
@@ -20,3 +20,22 @@ for iters in range (sampleTries):
 		heOpens = heOpens + 1				
 	#print "Host opens",heOpens
 
+	# switch to the other unopened door and play.
+	finalDoor = 0
+
+	if(switchDoors is True):
+		while (finalDoor == iOpen or finalDoor == heOpens):
+			finalDoor = finalDoor + 1
+        #print "You finally choose", iOpen
+
+	# check for a win
+	if (finalDoor == car):
+		#print "Won"
+		timesWon = timesWon + 1
+	else:
+		#print "Lost"
+		timesLost = timesLost + 1
+
+print "Times won", timesWon
+print "Times lost", timesLost
+print "Won", (100*timesWon/(timesWon+timesLost)), "%"
